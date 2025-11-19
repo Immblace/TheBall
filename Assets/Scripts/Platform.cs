24ff1131fba778b3f2c9943c4839902807c4e1d6
Farmagood <Immblace@gmail.com>
2025-11-20 00:16:58 +0400

    add touch control for platform

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     private Vector3 mousePos;
+    private Vector3 touchPos;
 
     private void OnEnable()
     {
@@ -15,6 +16,23 @@ public class Platform : MonoBehaviour
     }
 
     private void Update()
+    {
+        SwipeMove(); 
+        //MoveMouse(); 
+    }
+
+    private void SwipeMove()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            transform.position = new Vector3(touchPos.x, transform.position.y, transform.position.z);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -1.77f, 1.77f), transform.position.y, transform.position.z);
+        }
+    }
+
+    private void MoveMouse()
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(mousePos.x, transform.position.y, transform.position.z);
